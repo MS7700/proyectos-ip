e7ed7dd2368f390e5d894d6e58f93b369f09cf7a
Vlastos25 <noel525054@gmail.com>
2021-07-09 18:06:43 -0400

    Creación XML

---
+++ PracticaIII/CreacionXML/CreacionXML/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CreacionXML
+{
+    class Conexion
+    {
+        public static SqlConnection getConnection() {
+
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-11AN0PI\\SQLEXPRESS;Initial Catalog=practicaiii;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            con.Open();
+            return con;
+        
+        }
+    }
+}
---
+++ PracticaIII/CreacionXML/CreacionXML/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreacionXML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            writeXML();
+            MessageBox.Show("El archivo fue generado");
+        }
+        private void writeXML() {
+            SqlConnection con = Conexion.getConnection();
+            string sSQL = "select Asientos.noasiento, descripcion, fecha, Cuenta.nocuenta, DescripcionC, TipoMovimiento, Monto from movimiento\n" +
+            "inner join Asientos on movimiento.noasiento = Asientos.noasiento\n" +
+            "inner join Cuenta on movimiento.nocuenta = Cuenta.nocuenta";
+            SqlDataAdapter oda = new SqlDataAdapter(sSQL, con);
+            DataTable oTb = new DataTable();
+            oTb.TableName = "Asientos";
+            oda.Fill(oTb);
+            oTb.WriteXml(@"X:\Cuentas.xml", true);
+            con.Close();
+        }
+    }
+}
