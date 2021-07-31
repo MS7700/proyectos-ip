0a86b23b881c6b1fd4fa3e2eab3e2e463c82d5c0
Eduardo Haniel Duran Cuello <haniel1611@hotmail.com>
2021-07-31 11:17:15 -0400

    Cliente FTP

---
+++ PracticaVII/FTP_CLIENT/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FTP_CLIENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ftp cliente = new ftp(@"ftp://127.0.0.1/", "eduardo", "12345");
+            cliente.upload("klk.txt", @"C:\Users\hanie\Desktop\klk.txt");
+        }
+    }
+}
---
+++ PracticaVII/FTP_CLIENT/ftp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTP_CLIENT
+{
+    class ftp
+    {
+        private string usuario = null;
+        private string host = null;
+        private string password = null;
+        private FtpWebRequest ftpRequest = null;
+        //private FtpWebResponse ftpResponse = null;
+        private Stream ftpStream = null;
+        private int bufferSize = 2048;
+
+        public ftp(string IP, string userName, string pass)
+        {
+            usuario = userName;
+            host = IP;
+            password = pass;
+        }
+
+        public void upload(string remoteFile, string localFile)
+        {
+            try
+            {
+                /* Create an FTP Request */
+                ftpRequest = (FtpWebRequest)FtpWebRequest.Create(host + "/" + remoteFile);
+                /* Log in to the FTP Server with the User Name and Password Provided */
+                ftpRequest.Credentials = new NetworkCredential(usuario, password);
+                /* When in doubt, use these options */
+                ftpRequest.UseBinary = true;
+                ftpRequest.UsePassive = true;
+                ftpRequest.KeepAlive = true;
+                /* Specify the Type of FTP Request */
+                ftpRequest.Method = WebRequestMethods.Ftp.UploadFile;
+                /* Establish Return Communication with the FTP Server */
+                ftpStream = ftpRequest.GetRequestStream();
+                /* Open a File Stream to Read the File for Upload */
+                FileStream localFileStream = new FileStream(localFile, FileMode.Create);
+                /* Buffer for the Downloaded Data */
+                byte[] byteBuffer = new byte[bufferSize];
+                int bytesSent = localFileStream.Read(byteBuffer, 0, bufferSize);
+                /* Upload the File by Sending the Buffered Data Until the Transfer is Complete */
+                try
+                {
+                    while (bytesSent != 0)
+                    {
+                        ftpStream.Write(byteBuffer, 0, bytesSent);
+                        bytesSent = localFileStream.Read(byteBuffer, 0, bufferSize);
+                    }
+                }
+                catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+                /* Resource Cleanup */
+                localFileStream.Close();
+                ftpStream.Close();
+                ftpRequest = null;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+            return;
+        }
+    }
+}
